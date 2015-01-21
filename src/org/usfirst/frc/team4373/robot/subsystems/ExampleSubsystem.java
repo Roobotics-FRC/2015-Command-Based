eda7d85c23d8e2583befd6e6b53d56bf9ebfb9af
lumpyrocks <15doriank@abingtonfriends.net>
2015-01-21 15:58:39 -0500

    HOW DO SUBSYSTEMS WORK?

--- src/org/usfirst/frc/team4373/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team4373/robot/subsystems/ExampleSubsystem.java
@@ -1,7 +1,10 @@
 
 package org.usfirst.frc.team4373.robot.subsystems;
 
+import org.usfirst.frc.team4373.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Gyro;
 
 /**
  *
@@ -11,9 +14,20 @@ public class ExampleSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	private Gyro gyro;
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void initialize () {
+    	//please can we find out where this is actually supposed to go
+    	gyro = new Gyro (RobotMap.gyroPort);
+    }
+    
+    public double get(){
+    	return (gyro.getAngle() % 360);
     }
 }
 
