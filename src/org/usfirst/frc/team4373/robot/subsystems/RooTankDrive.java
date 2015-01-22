25d9ace378dbdee5f353f076b94d0b6304710813
lumpyrocks <15doriank@abingtonfriends.net>
2015-01-22 16:10:49 -0500

    123456789

--- src/org/usfirst/frc/team4373/robot/subsystems/RooTankDrive.java
+++ src/org/usfirst/frc/team4373/robot/subsystems/RooTankDrive.java
@@ -1,13 +1,42 @@
 package org.usfirst.frc.team4373.robot.subsystems;
 
+import org.usfirst.frc.team4373.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class RooTankDrive extends Subsystem {
-
+	
+	Talon left;
+	Talon right;
+	public RooTankDrive(){
+		super();
+		left = new Talon (RobotMap.leftMotorPort);
+		right = new Talon (RobotMap.rightMotorPort);
+	}
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void setLeft (double foo){
+		left.set(foo);
+	}
+	
+	public void setRight (double foo){
+		right.set(foo);
+	}
+	
+	public void setBoth (double foo){
+		setLeft (foo);
+		setRight (foo);
+	}
+	
+	public void stop (){
+		setBoth (0);
+	}
+	
 
 }
