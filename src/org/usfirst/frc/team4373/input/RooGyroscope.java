e155858e0b56bdaa37eeda0092dc131d461de1ae
Henry Pitcairn <735Tesla@gmail.com>
2015-01-24 17:38:24 -0500

    init angle

--- src/org/usfirst/frc/team4373/input/RooGyroscope.java
+++ src/org/usfirst/frc/team4373/input/RooGyroscope.java
@@ -3,14 +3,15 @@ package org.usfirst.frc.team4373.input;
 import edu.wpi.first.wpilibj.Gyro;
 
 public class RooGyroscope extends Gyro {
-
+	// Used to compensate for the fact the Gryo doesn't always start at zero
+	private double initAngle;
 	public RooGyroscope(int channel) {
 		super(channel);
-		// TODO Auto-generated constructor stub
+		initAngle = this.getAngle();
 	}
 	
 	public double rooGetAngle () {
-		double angle = this.getAngle();
+		double angle = this.getAngle()-initAngle;
 		angle = angle % (Math.abs(angle)/angle)*360;
 		if (angle < -180)
 			return angle + 360;
